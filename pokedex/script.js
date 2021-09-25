1ec5e9ff60bb5a3a078565fc42c26835f4b3d9f5
Mateus Turola <turolamateus@gmail.com>
2021-09-25 16:16:32 -0300

    inicia coleçao

--- pokedex/script.js
+++ pokedex/script.js
@@ -4,11 +4,16 @@ const levelInput = document.querySelector('#level');
 const attackInput = document.querySelector('#attack');
 const defenseInput = document.querySelector('#defense');
 const captureButton = document.querySelector('#capture');
+const ColectionButton = document.querySelector('#bnt-colection');
+
+//CHAMA POKEMON COLLECTION
+const titleCol = document.querySelector('#title-collection');
+const textCol = document.querySelector('#text-collection');
+const myCol = document.querySelector('#my-pokemon');
+
 
 /* Crie uma função que recebe Nome, Level, Ataque e Defesa e guarda essas informações em um objeto */
-
 const pokedex = [];
- 
 
 const camera = () => {
   pokedex.push({
@@ -28,19 +33,28 @@ camera();
 })
 
     
-
+// EXIBIR COLEÇÃO
 
 /* Crie uma função que exiba as informações de todos os pokemons em memória no seguinte formato: "Nome - Level - Ataque - Defesa" */
-
 const openPokedex = () => {
   for (let index = 0; index < pokedex.length; index +=1) {
   const pokemons = Object.entries(pokedex[index]);
     for (key in pokemons) {
-        console.log(`${pokemons[key][0]}:${pokemons[key][1]}`);
+        `${pokemons[key][0]}:${pokemons[key][1]}`;
       }  
-    console.log('--------');
+    '--------';
   }
   
 }
 
-openPokedex();
+// CRIAR TITULO E TEXTO
+const createTitle = () => {
+  const coachInput = document.querySelector('#coach');
+
+  titleCol.innerHTML = `Coleção do ${coachInput}`;
+  textCol.innerHTML = 'sasdasdadlaksda asdmasdakdmas'
+  myCol.innerHTML = openPokedex();
+}
+
+
+ColectionButton.addEventListener('click',createTitle() )
