738c095fcdfa76fb20bf5adfd9fd2f3646825263
Mateus Turola <turolamateus@gmail.com>
2021-09-25 15:55:13 -0300

    Adiciona dinamismo ao formulario

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,35 +1,36 @@
-/* Feature 1 - Quando apontamos a câmera da Pokedex para um Pokemon precisamos guardar as informações do nosso monstrinho na memória da  Pokedex.
-As informações são:
-Nome - String,
-Level - um inteiro entre 1 e 50,
-Ataque - um inteiro entre 1 e 345,
-Defesa - um inteiro entre 1 e 400
-Crie uma função que recebe Nome, Level, Ataque e Defesa e guarda essas informações em um objeto */
+// CHAMA CAMPOS DO FORMULÁRIO
+const nameInput = document.querySelector('#name');
+const levelInput = document.querySelector('#level');
+const attackInput = document.querySelector('#attack');
+const defenseInput = document.querySelector('#defense');
+const captureButton = document.querySelector('#capture');
+
+/* Crie uma função que recebe Nome, Level, Ataque e Defesa e guarda essas informações em um objeto */
 
 const pokedex = [];
+ 
 
-const camera = (name, lev, attack, defense) => {
+const camera = () => {
   pokedex.push({
-    Nome: name,
-    Level: lev,
-    Ataque: attack,
-    Defesa: defense });
+    Nome: nameInput.value,
+    Level: levelInput.value,
+    Ataque: attackInput.value,
+    Defesa: defenseInput.value });
+    alert(`${nameInput.value} Capturado!`)
+    console.log(pokedex)
   return pokedex;
 };
 
-const namePok = 'NOME DO POKEMON';
-const lev = 150;
-const attack = 1345;
-const defense = 1400; 
+
+captureButton.addEventListener('click', (event) =>{
+event.preventDefault();
+camera();
+})
+
+    
 
 
-camera(namePok, lev, attack, defense);
-
-
-/*
-Feature 2 - A pokedex vem equipada com um botão para exibir os pokemons que estão salvos na memória da pokedex.
-Crie uma função que exiba as informações de todos os pokemons em memória no seguinte formato:
-"Nome - Level - Ataque - Defesa" */
+/* Crie uma função que exiba as informações de todos os pokemons em memória no seguinte formato: "Nome - Level - Ataque - Defesa" */
 
 const openPokedex = () => {
   for (let index = 0; index < pokedex.length; index +=1) {
